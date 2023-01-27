c3185d781a75cf480fb0a9bf360c328a63cabaf2
bayuraditya <bayuraditya35@gmail.com>
2023-01-27 16:51:25 +0800

    ganti ke date

--- inventoryProject/inventories/models.py
+++ inventoryProject/inventories/models.py
@@ -35,7 +35,7 @@ class Staff(models.Model):
 class MaintenanceInventories(models.Model):
     id = models.IntegerField(primary_key=True, unique=True, null=False)
     inventory = models.ForeignKey(Inventories, on_delete=models.CASCADE)
-    maintenanceDate = models.DateTimeField( blank=False)
+    maintenanceDate = models.DateField( blank=False)
     maintenanceVendor = models.CharField(max_length=100, null=False, blank=False)
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
     
